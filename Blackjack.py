8d5968ada6c3ef80bc01cead4bffa07a356cf333
Diego Vargas <diegofvargas@gmail.com>
2018-06-02 20:09:34 -0400

    added a few classes

--- Blackjack.py
+++ Blackjack.py
@@ -6,19 +6,19 @@ ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
 
-class Card():
+class Card:
     def __init__(self,suit,rank):
         self.suit = suit
         self.rank = rank
     def __str__(self):
-        print (f'{self.rank} of {self.suit}')
+        return f"{self.rank} of {self.suit}"
 
-class Deck():
+class Deck:
     def __init__(self):
         self.deck = []
         for suit in suits:
             for rank in ranks:
-                self.deck.append(suit[rank])
+                self.deck.append(Card(suit,rank))
     def __str__(self):
         for item in self.deck:
             print (item)
@@ -26,6 +26,21 @@ class Deck():
         random.shuffle(self.deck)
     def deal(self):
         pass
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+    def add_card(self,card):
+        self.card = card
+        self.cards.append(self.card)
+        self.value += values[self.card.rank]
+        if self.card.rank == 'Ace':
+            self.aces+=1
+    def adjust_for_ace(self):
+        if self.aces>0 and self.value>21:
+            self.value-=10
 
 test_deck = Deck()
 print(test_deck)
